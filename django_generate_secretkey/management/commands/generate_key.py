dd7845cd62a12faaed4a09ed2199d53084b45428
Michael Klich <michal@michalklich.com>
2015-06-23 12:26:23 +0100

    Add replacing SECRET_KEY in file

--- django_generate_secretkey/management/commands/generate_key.py
+++ django_generate_secretkey/management/commands/generate_key.py
@@ -1,21 +1,24 @@
 import re
+import os.path
 from tempfile import mkstemp
 from shutil import move
-from os import remove, close
+from os import remove, close, linesep
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
+
+
+SK = "SECRET_KEY = '%s'" + linesep
 
 
 class Command(BaseCommand):
     help = 'Generate SECRET_KEY and save it in settings file'
 
     def add_arguments(self, parser):
-        # TODO: verify if these flags are available
         parser.add_argument('-p', '--print', action='store_true',
                             help=('Print key to stdout instead saving in '
                                   'settings file.'))
-        parser.add_argument('-s', '--show', action='store_true')
-        parser.add_argument('--path', help='Path to settings file, so simple')
+        parser.add_argument('path', nargs='?',
+                            help='Save key in settings file, provide path')
 
     def handle(self, *args, **options):
         # Create a random SECRET_KEY to put it in the main settings.
@@ -25,14 +28,16 @@ class Command(BaseCommand):
         # TODO: kill these elif's
         if options['print']:
             self.stdout.write(rnd_str)
-        elif options['show']:
-            self.stdout.write("SECRET_KEY='%s'" % rnd_str)
         elif options['path']:
-            self._replace(options['path'], r'^SECRET_KEY ?=',
-                          "SECRET_KEY='%s'" % rnd_str)
-            self.stdout.write('saving')
+            if os.path.exists(options['path']):
+                self._replace(options['path'], r'^SECRET_KEY ?=',
+                              SK % rnd_str)
+            else:
+                self.stdout.write('File %s not found.' % options['path'])
+        else:
+            self.stdout.write(SK % rnd_str)
 
-    def _replace(file_path, pattern, subst):
+    def _replace(self, file_path, pattern, subst):
         fh, abs_path = mkstemp()
         with open(abs_path, 'w') as new_file:
             with open(file_path) as old_file:
