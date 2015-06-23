15e538d1c46dfe68d762ab40012da17a3cdf6dc2
Michael Klich <michal@michalklich.com>
2015-06-23 11:44:33 +0100

    Add replacing value in settings file

--- README.rst
+++ README.rst
@@ -1,2 +1,7 @@
 # django-generate-secretkey
 Generates Django SECRET_KEY with management command and adds to settings.
+
+Very composable in deployment scripts or other automation.
+
+python manage.py generate_key -s >> settings.py
+python manage.py generate_key settings.py
--- django_generate_secretkey/management/commands/generate_key.py
+++ django_generate_secretkey/management/commands/generate_key.py
@@ -1,3 +1,7 @@
+import re
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
@@ -11,14 +15,33 @@ class Command(BaseCommand):
                             help=('Print key to stdout instead saving in '
                                   'settings file.'))
         parser.add_argument('-s', '--show', action='store_true')
+        parser.add_argument('--path', help='Path to settings file, so simple')
 
     def handle(self, *args, **options):
         # Create a random SECRET_KEY to put it in the main settings.
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         rnd_str = get_random_string(50, chars)
+
+        # TODO: kill these elif's
         if options['print']:
             self.stdout.write(rnd_str)
         elif options['show']:
             self.stdout.write("SECRET_KEY='%s'" % rnd_str)
-        else:
+        elif options['path']:
+            self._replace(options['path'], r'^SECRET_KEY ?=',
+                          "SECRET_KEY='%s'" % rnd_str)
             self.stdout.write('saving')
+
+    def _replace(file_path, pattern, subst):
+        fh, abs_path = mkstemp()
+        with open(abs_path, 'w') as new_file:
+            with open(file_path) as old_file:
+                for line in old_file:
+                    secret_key = re.match(r'^SECRET_KEY ?=', line)
+                    if secret_key:
+                        line = subst
+                    new_file.write(line)
+
+        close(fh)
+        remove(file_path)
+        move(abs_path, file_path)
